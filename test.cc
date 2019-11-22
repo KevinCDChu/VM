bf52991fa97329531e32f426f2cecfdfa9725582
rholterhus <rileyholterhus@gmail.com>
2019-11-22 11:58:01 -0500

    kinda added scrolling

--- test.cc
+++ test.cc
@@ -1,8 +1,12 @@
 #include <ncurses.h>
 #include <iostream>
 #include <fstream>
+
 int main() {
+    size_t x = 0;
+    size_t y = 0;
     initscr();
+    scrollok(stdscr, true);
     cbreak();
     noecho();
     std::ifstream f{"test.cc"};
@@ -10,10 +14,26 @@ int main() {
     char c;
     std::string str;
     while(f >> c) {
-        if(c != '\n') str += c;
+        str += c;
     }
 	printw(str.c_str());
-	refresh();	
-	getch();
+    wmove(stdscr, y, x);
+	refresh();
+    int ch = getch();
+    while(ch != 'p') {
+        ch = getch();
+        if(ch == 'w') scrl(-1);
+        if(ch == 's') scrl(1);
+        if(ch == 'h') x -= 1;
+        if(ch == 'k') y -= 1;
+        if(ch == 'j') y += 1;
+        if(ch == 'l') x += 1;
+        if(x < 1) x = 0;
+        if(y < 1) y = 0;
+        wmove(stdscr, y, x);
+        // printw(str.c_str());
+        refresh();
+    }
 	endwin();
     }
+
