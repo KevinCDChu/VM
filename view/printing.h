5427289991b0ff95c13140f0d39d73c5c5175f15
rholterhus <rileyholterhus@gmail.com>
2019-11-25 23:03:21 -0500

    preprocessor start

--- view/printing.h
+++ view/printing.h
@@ -19,7 +19,8 @@ void init_colours(bool code_file) {
 
 
 void myprintw(std::string line) {
-    std::regex preprocessor("^( )*#( )*include( )*<.+>|^( )*#( )*include( )*\".+\"|^( )*#( )*include( )*<.+>|^( )*#( )*include( )*\".+\"");
+    if(line == "") return;
+    std::regex preprocessor("^( )*#( )*include( )*<.+>|^( )*#( )*include( )*\".+\"");
     std::smatch match_preprocessor;
     if(line.find("//") != std::string::npos) { //contains comment
         int comment_idx = line.find("//");
@@ -32,8 +33,7 @@ void myprintw(std::string line) {
         printw(match_preprocessor[0]);
         attroff(COLOR_PAIR(4));
         size_t match_start = line.find(match_preprocessor[0]);
-        printw(line.substr(0, std::min(match_start + match_preprocessor[0].length(), line.length())));
-        //myprintw(line.substr(0, index_after));
+        myprintw(line.substr(match_start + match_preprocessor[0].length(), line.length() - (match_start + match_preprocessor[0].length())));
     } else {
         printw(line);
     }
