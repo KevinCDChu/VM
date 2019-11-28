622185a895cc856771aaf324a83cf36906e0b3a7
rholterhus <rileyholterhus@gmail.com>
2019-11-28 00:07:07 -0500

    made colours unique

--- model/model.h
+++ model/model.h
@@ -537,12 +537,12 @@ class Logic : public Model {
             }
             else if (cmd == 't') {
                 savecursor();
-                if(!cmdt(curline, end, ch)) {
+                if(!cmdf(curline, end, ch)) {
                     returncursor();
                     return;
                 }
                 for(int i = 1; i < repeats; ++i) {
-                    if(!cmdt(curline, end, ch)) {
+                    if(!cmdf(curline, end, ch)) {
                         returncursor();
                         return;
                     }
